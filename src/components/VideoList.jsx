f077473a66eaa52f483cd47b02874e96cb107760
stephaniesara <stephaniesarachong@gmail.com>
2018-01-31 17:11:07 -0800

    Implement App, VideoPlayer, VideoList and VideoEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map(video => 
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
 
@@ -16,4 +14,5 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
+// <div><h5><em>videoListEntry</em>{props.videos[0]}</h5></div>
 window.VideoList = VideoList;
